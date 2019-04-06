4b1c5e83a47675f2b341a1c01fe9692c8c9ee359
Huy Vo <huyvanvo94@gmail.com>
2019-04-05 20:06:09 -0700

    update robin.py

--- load_balancer/robin.py
+++ load_balancer/robin.py
@@ -8,7 +8,7 @@ class RoundRobin:
 
     def __init__(self, nodes=[]):
         self.nodes = nodes
-        self.count = 0
+        self.index = 0
 
     def add(self, node):
         self.nodes.append(node)
@@ -16,16 +16,13 @@ class RoundRobin:
     def remove(self, node):
         # Reset count to zero
         self.nodes.remove(node)
-        self.count = 0
+        self.index = 0
 
     def get(self):
-        node = self.nodes[self.count]
-        self.count = (self.count + 1) % (len(self.nodes))
+        node = self.nodes[self.index]
+        self.index = (self.index + 1) % (len(self.nodes))
         return node
 
-    def set(self, nodes=[]):
-        self.nodes = nodes
-        self.count = 0
 
 if __name__ == '__main__':
     rr = RoundRobin(nodes=[('localhost', i) for i in range(5000, 5005)])
